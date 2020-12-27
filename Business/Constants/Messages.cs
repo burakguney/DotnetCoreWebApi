a518184b8fb4f54eacd16ff72354dadd5e3e5f07
BurakGuney <59474220+burakguney@users.noreply.github.com>
2020-12-28 01:21:07 +0300

    Business Layer create access token

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,5 +8,13 @@ namespace Business.Constants
         public static string ProductUpdatedMessage = "Ürün başarıyla güncellendi.";
         public static string ProductDeletedMessage = "Ürün başarıyla silindi.";
         public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Şifre Hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+
+        public static string UserAlreadyExists = "Bu e-posta adresi kullanılmış!";
+
+        public static string UserRegistered = "Kullanıcı başarıyla oluşturuldu.";
+
+        public static string AccessTokenCreated = "Access Token başarıyla oluşturuldu.";
     }
 }
